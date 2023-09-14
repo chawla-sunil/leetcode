6c97ec1a4af98de8dd5aa7340738579184bcf2ba
chawla-sunil <108911627+chawla-sunil@users.noreply.github.com>
2023-09-14 18:34:36 +0530

    Create LC19RemoveNthFromEnd.java

---
+++ topInterviewQuestions/LC19RemoveNthFromEnd.java
@@ -0,0 +1,50 @@
+
+public class LC19RemoveNthFromEnd {
+
+//    Given the head of a linked list, remove the nth node from the end of the list and return its head.
+//
+//    Example 1:
+//    Input: head = [1,2,3,4,5], n = 2
+//    Output: [1,2,3,5]
+//    Example 2:
+//
+//    Input: head = [1], n = 1
+//    Output: []
+//    Example 3:
+//
+//    Input: head = [1,2], n = 1
+//    Output: [1]
+//
+//    Constraints:
+//    The number of nodes in the list is sz.
+//            1 <= sz <= 30
+//            0 <= Node.val <= 100
+//            1 <= n <= sz
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head.next == null) {return null;}
+        ListNode curr = head;
+        int total = 0;
+        while(curr != null) {
+            total += 1;
+            curr = curr.next;
+        }
+        if (total == n) {return head.next;}
+        curr = head;
+
+        for (int i = 1; i < total - n ; i++) {
+            curr = curr.next;
+        }
+        ListNode newNode = curr.next;
+        curr.next = newNode.next;
+        return head;
+    }
+
+     private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
