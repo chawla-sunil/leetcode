4e5b5384dcd3c2a153d8c3b9b22fd3983422c508
chawla-sunil <108911627+chawla-sunil@users.noreply.github.com>
2023-09-14 18:36:05 +0530

    Update LC19RemoveNthFromEnd.java

--- topInterviewQuestions/LC19RemoveNthFromEnd.java
+++ topInterviewQuestions/LC19RemoveNthFromEnd.java
@@ -47,4 +47,16 @@ public class LC19RemoveNthFromEnd {
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
+
+    public ListNode removeNthFromEndAnotherMethodInOneTraversal(ListNode head, int n) {
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n; i++) fast = fast.next;
+        if (fast == null) return head.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
 }
