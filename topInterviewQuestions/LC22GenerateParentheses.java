ab6874dcec0cdd070e2b12f2ac37ce538f7c7ed7
chawla-sunil <108911627+chawla-sunil@users.noreply.github.com>
2023-09-15 00:32:46 +0530

    Create LC22GenerateParentheses.java

---
+++ topInterviewQuestions/LC22GenerateParentheses.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class LC22GenerateParentheses {
+//    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+//
+//    Example 1:
+//
+//    Input: n = 3
+//    Output: ["((()))","(()())","(())()","()(())","()()()"]
+//
+//    Example 2:
+//    Input: n = 1
+//    Output: ["()"]
+//
+//    Constraints:
+//
+//     1 <= n <= 8
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        recurse(res, 0, 0, "", n);
+        return res;
+    }
+
+    public void recurse(List<String> res, int openBracket, int closeBracket, String s, int n) {
+        if (s.length() == n * 2) {
+            res.add(s);
+            return;
+        }
+        if (openBracket < n) {
+            recurse(res, openBracket + 1, closeBracket, s + "(", n);
+        }
+        if (closeBracket < openBracket) {
+            recurse(res, openBracket, closeBracket + 1, s + ")", n);
+        }
+    }
+}
