3a55363c1c690e5661848b865826627a6c7d410c
chawla-sunil <108911627+chawla-sunil@users.noreply.github.com>
2023-10-03 20:53:49 +0530

    Create LC102BinaryTreeLevelOrderTraversal.java

---
+++ topInterviewQuestions/LC102BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,85 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LC102BinaryTreeLevelOrderTraversal {
+//    Given the root of a binary tree, return the level order traversal of its nodes' values.
+//    (i.e., from left to right, level by level).
+//
+//    Example 1:
+//    Input: root = [3,9,20,null,null,15,7]
+//    Output: [[3],[9,20],[15,7]]
+//
+//    Example 2:
+//    Input: root = [1]
+//    Output: [[1]]
+//
+//    Example 3:
+//    Input: root = []
+//    Output: []
+//
+//
+//    Constraints:
+//
+//            The number of nodes in the tree is in the range [0, 2000].
+//            -1000 <= Node.val <= 1000
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        int height = height(root);
+        for (int i = 0; i < height; i++) {
+            ans.add(new ArrayList<>());
+        }
+        // List<Integer> firstList = new ArrayList<>();
+        Queue<NodeWithLevel> q = new LinkedList<>();
+        q.offer(new NodeWithLevel(0, root));
+
+        while (!q.isEmpty()) {
+            NodeWithLevel node = q.poll();
+            ans.get(node.level).add(node.node.val);
+            if (node.node.left!= null) {
+                q.offer(new NodeWithLevel(node.level + 1, node.node.left));
+            }
+            if (node.node.right != null) {
+                q.offer(new NodeWithLevel(node.level + 1, node.node.right));
+            }
+        }
+        return ans;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class NodeWithLevel{
+        int level;
+        TreeNode node;
+        NodeWithLevel() {}
+        NodeWithLevel(int level, TreeNode node) {
+            this.level = level;
+            this.node = node;
+        }
+    }
+}
