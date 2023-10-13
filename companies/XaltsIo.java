f262e887b93bd94f290e88a07d4eaddd26192ef3
chawla-sunil <108911627+chawla-sunil@users.noreply.github.com>
2023-10-13 18:59:54 +0530

    Create XaltsIo.java

---
+++ companies/XaltsIo.java
@@ -0,0 +1,19 @@
+
+public class XaltsIo {
+//    xalt.io
+//
+//for(let i = 0; i < 5; i++){
+//        setTimeout(()=>{
+//                console.log(i);
+//  }, (i+1)*1000);
+//    }
+//
+//    what is jwt and its signature
+//    hashing vs encryption
+//
+//    Promises vs callback
+//    event-loop
+//
+//    REST methods GET, POST, ...
+
+}
